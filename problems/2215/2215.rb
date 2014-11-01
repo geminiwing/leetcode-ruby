cb2bc790bdd6e70c1cda4522db5e746624a3a9ac
William Kim <geminiwing@users.noreply.github.com>
2014-11-01 01:16:29 -0700

    add tests

--- problems/2215/2215.rb
+++ problems/2215/2215.rb
@@ -7,3 +7,13 @@ def find_difference(nums1, nums2)
   ans1 = (nums2 - nums1)
   [ans0, ans1]
 end
+
+# test
+
+nums1 = [1,2,3]
+nums2 = [2,4,6]
+p find_difference(nums1, nums2)  # Output: [[1,3],[4,6]]
+
+nums1 = [1,2,3,3]
+nums2 = [1,1,2,2]
+p find_difference(nums1, nums2)  # Output: [[3],[]]
