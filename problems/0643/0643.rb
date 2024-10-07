cbb1634970ab579a86685abaabb5e8ee73127f68
William Kim <geminiwing@users.noreply.github.com>
2024-10-07 01:23:45 -0700

    solution for 0643

---
+++ problems/0643/0643.rb
@@ -0,0 +1,27 @@
+# 0643. Maximum Average Subarray I - Easy
+
+def find_max_average(nums, k)
+  i1 = 0       # window beginning index
+  i2 = k - 1   # window ending index
+  len = nums.length
+  top_sum = 0
+  sum = 0
+
+  # find the sum of the first k elements
+  (i1..i2).each do |i|
+    sum += nums[i]
+  end
+  puts sum
+
+  top_sum = sum
+
+  i2 += 1
+  while i2 < len do
+    sum = sum + nums[i2] - nums[i1]
+    top_sum = sum if top_sum < sum
+    i1 += 1
+    i2 += 1
+  end
+
+  return top_sum.to_f / k
+end
