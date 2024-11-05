e5fb96b791809e92b4cb32d865c74700bd48ec97
William Kim <geminiwing@users.noreply.github.com>
2024-11-05 00:12:35 -0800

    solution for 2390

---
+++ problems/2390/2390.rb
@@ -0,0 +1,18 @@
+# 2390. Remove Stars - Medium
+def remove_stars(s)
+  stack = []
+  s.each_char do |c|
+    if c == '*'
+      stack.pop unless stack.empty?
+    else
+      stack << c
+    end
+  end
+
+  result = ""
+  for c in stack do
+    result << c
+  end
+
+  result
+end
