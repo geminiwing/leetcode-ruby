5848b6e66b43cf865c35530d1e28b64233aac1f5
William Kim <geminiwing@users.noreply.github.com>
2024-11-20 00:32:26 -0800

    add tests

--- problems/0206/0206.rb
+++ problems/0206/0206.rb
@@ -17,3 +17,31 @@ def reverse_list(head)
 
   return p1
 end
+
+# test
+
+class ListNode
+  attr_accessor :val, :next
+
+  def initialize(val = 0, _next = nil)
+    @val = val
+    @next = _next
+  end
+end
+
+n5 = ListNode.new(5, nil)
+n4 = ListNode.new(4, n5)
+n3 = ListNode.new(3, n4)
+n2 = ListNode.new(2, n3)
+n1 = ListNode.new(1, n2)
+head = n1
+
+new_head = reverse_list(head)
+
+p = new_head
+while(p != nil)
+  puts p.val
+  p = p.next
+end
+
+# Output: 5 4 3 2 1
