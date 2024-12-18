04e7302cf4d3d62ab45aca1d6f51d6c149e7db92
William Kim <geminiwing@users.noreply.github.com>
2024-12-18 00:50:33 -0800

    extract binary tree builder

---
+++ problems/lib/binary_tree.rb
@@ -0,0 +1,38 @@
+module BinaryTree
+  class TreeNode
+    attr_accessor :val, :left, :right
+
+    def initialize(val = 0, left = nil, right = nil)
+        @val = val
+        @left = left
+        @right = right
+    end
+  end
+
+  # Construct a binary tree from a sparse array
+  def build(arr)
+    return nil if arr.empty?
+
+    root = TreeNode.new(arr.shift)
+    q = [root]
+
+    while !arr.empty?
+      node = q.shift
+      left_val = arr.shift
+      right_val = arr.shift
+
+      unless left_val.nil?
+        node.left = TreeNode.new(left_val)
+        q << node.left
+      end
+
+      unless right_val.nil?
+        node.right = TreeNode.new(right_val)
+        q << node.right
+      end
+    end
+
+    root
+  end
+  module_function :build
+end
