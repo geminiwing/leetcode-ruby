dee3fe300067c333bd183f0ed71e87a0d9d3b358
William Kim <geminiwing@users.noreply.github.com>
2025-01-03 01:12:44 -0800

    add tests

--- problems/0700/0700.rb
+++ problems/0700/0700.rb
@@ -1,4 +1,5 @@
 # 700. Search in a Binary Search Tree - Easy
+require_relative '../lib/binary_tree'
 
 def search_bst(root, val)
   return root if root == nil
@@ -10,3 +11,14 @@ def search_bst(root, val)
     search_bst(root.right, val)
   end
 end
+
+# test
+
+input = [4,2,7,1,3]
+root = BinaryTree.build(input)
+
+node = search_bst(root, 2)
+puts node&.val # Output: 2
+
+node = search_bst(root, 5)
+puts node&.val # Output: nil
