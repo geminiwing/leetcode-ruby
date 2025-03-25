36c867a38497a137699c1b08f5a38c57294de2d4
William Kim <geminiwing@users.noreply.github.com>
2025-03-24 23:48:47 -0700

    add alternative solution with tests

--- problems/2591/2591.rb
+++ problems/2591/2591.rb
@@ -44,3 +44,73 @@ end
 def distributable?(money, children)
   children == 1 && money == 4 ? false : true
 end
+
+# Alternative solution
+# This solution shows the status of the dishes for each child for debugging purposes.
+def dist_money(money, children)
+  return -1 if money < children
+
+  # give each child 1 dollar, for everyone must receive some money.
+  dishes = Array.new(children, 1)
+  money -= children
+
+  # distribute the rest of the money
+  children.times do |i|
+    break if money == 0
+
+    if i != children - 1 # for non-last child
+      if money > 7
+        dishes[i] += 7
+        money -= 7
+      else
+        dishes[i] += money
+        money = 0
+      end
+    else                 # for the last child
+      if money == 3
+        dishes[i] += money + 1  # avoid giving 4 dollars
+        dishes[i-1] -= 1
+      else
+        dishes[i] += money
+      end
+      money = 0
+    end
+  end
+
+  puts dishes.to_s       # for debugging
+  return dishes.count(8)
+end
+
+# Tests
+
+money = 20
+children = 3
+puts dist_money(money, children) # 1
+
+money = 16
+children = 2
+puts dist_money(money, children) # 2
+
+money = 2
+children = 2
+puts dist_money(money, children) # 0
+
+money = 1
+children = 2
+puts dist_money(money, children) # -1
+
+money = 8
+children = 2
+puts dist_money(money, children) # 0
+
+money = 17
+children = 4
+puts dist_money(money, children) # 1
+
+money = 24
+children = 2
+puts dist_money(money, children) # 1
+
+money = 16
+children = 10
+puts dist_money(money, children) # 0
